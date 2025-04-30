5030e7a6d25b19e312614d4aa133b63acd7f24e6
waeeelhajji <waelhajji.contact@gmail.com>
2025-04-30 21:30:52 +0100

    Week1Day2

---
+++ Week1/Day1/fun.js
@@ -0,0 +1,31 @@
+// console.log("hello")
+
+// ES5
+var MernStackNinjas = "Welcome to our New Javascript"
+
+// ES6
+const newStudents = "Welcome to the Best stack"
+let theNewStudents = "be a great dev"
+
+
+//~ SCOPE
+var name = "bob"
+
+function SayHi() {
+    name = "alice"
+    console.log(name)
+
+}
+
+SayHi()//?
+console.log(name)//? 
+for (var let = 0; index < 7; index++) {
+
+
+}
+console.log(index)
+
+
+//~---- HOISTING ----
+console.log(x)
+const x = 5
